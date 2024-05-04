de53db3741c50682f7f20dcaf080346c77297c5d
Eldodooo <fedaa_osama@icloud.com>
2024-05-04 12:09:13 +0300

    initial commit

---
+++ app/src/main/java/com/example/islami_v1/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.islami_v1
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.islami_v1.fragments.HomeFragment
+import com.example.islami_v1.fragments.ProfileFragment
+
+class MainActivity : AppCompatActivity() {
+    lateinit var homeTabTextView : TextView
+    lateinit var profileTabTextView : TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        homeTabTextView = findViewById(R.id.home_tab)
+        profileTabTextView = findViewById(R.id.profile_tab)
+
+        homeTabTextView.setOnClickListener{
+            // show or hide or replace fragment you need supportFragmentManger
+           /* supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container,HomeFragment())
+                .addToBackStack("Home")
+                .commit()*/
+            pushFragment(HomeFragment())
+
+
+        }
+        profileTabTextView.setOnClickListener(){
+            pushFragment(ProfileFragment())
+
+        }
+    }
+    fun pushFragment(fragment:Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container,fragment).addToBackStack("null").commit()
+
+    }
+}
---
+++ app/src/main/java/com/example/islami_v1/fragments/HomeFragment.kt
@@ -0,0 +1,26 @@
+package com.example.islami_v1.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.islami_v1.R
+
+class HomeFragment : Fragment() {
+    lateinit var homeText:TextView
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        homeText = view.findViewById(R.id.home_text)
+    }
+
+}
